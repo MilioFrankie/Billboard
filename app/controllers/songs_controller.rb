f870de4c377e37d257426fe8973cdd19b8d4bae0
Emilio <em.francomartinez@gmail.com>
2018-11-18 19:02:44 -0700

    updated songs controller, updated artist and songs view, seeded data for song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,26 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+  before_action :set_song
+  
   def index
+    @songs = Song.all
   end
 
   def show
   end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:artist_id])
+  end
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+  
+  def song_params
+    params.require(:song).permit(:name)
+  end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-10.times do 
-  Artist.create(
+20.times do 
+  artist = Artist.create(
     name: Faker::Music.band
   )
+  10.times do 
+    artist.songs.create(
+      name: Faker::Book.title
+    )
+  end 
 end
 print `clear`
-puts "10 bands created"
+puts "20 bands created"
+puts "10 song created per artist"
